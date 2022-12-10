df33e4ddbb321a878d0b46ddb908a91b23cf284e
Harunur5252 <hroshid695@gmil.com>
2022-12-10 21:38:45 +0600

    footer,about,refand,privacyPolicy,terms single type collection and contact multiple collection create

---
+++ src/api/refand/controllers/refand.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * refand controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::refand.refand');
---
+++ src/api/refand/routes/refand.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * refand router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::refand.refand');
